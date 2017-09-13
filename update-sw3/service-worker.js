d56acd57c0ebe6a18acc3bc176dca20f67d4ea08
liuxiang <lx98768@alibaba-inc.com>
2017-09-13 10:39:00 +0800

    sw reg

---
+++ update-sw3/service-worker.js
@@ -0,0 +1,34 @@
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    if(self.skipWaiting){
+        console.log('[ServiceWorker] begin skipWaiting');
+        self.skipWaiting();
+        console.log('[ServiceWorker] end skipWaiting');
+    }
+    //console.log("Test Install >" + "哈哈哈" + new Object());
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    //console.log('[ServiceWorker] begin self.clients.claim');
+    //setTimeout( self.clients.claim(), 10000);
+    // setTimeout(
+    //     e.waitUntil(caches.open("CACHE_NAME_TEST").then(function(e) {
+    //     return e.keys().then(function(t) {
+    //         return Promise.all(t.map(function(t) {
+    //             return e["delete"](t)
+    //         }))
+    //     })
+    // }).then(function() {
+    //         console.log('[ServiceWorker] call self.clients.claim');
+    //         return self.clients.claim();    
+    // }))
+    // , 10000);
+    //console.log('[ServiceWorker] end self.clients.claim');
+    //console.log("Test Activate >" + "哈哈哈" + new Object());
+});
+
+//self.addEventListener('fetch', function(e) {
+//    console.log('[Service Worker] Fetch', e.request.url);
+//    console.log("Test Fetch >" + "哈%#$哈哈" + new Object())
+//});
